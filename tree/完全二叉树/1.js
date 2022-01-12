eea60c56c71bd0779b02da3e1526ba3bbf4abad5
wudan <xes@wudandeMacBook-Pro.local>
2022-01-12 18:41:33 +0800

    add

---
+++ tree/完全二叉树/1.js
@@ -0,0 +1,23 @@
+/*
+ * @LastEditors: wudan01
+ * @description: 文件描述
+ */
+/**
+ * 取出堆顶元素（删除操作）
+ */
+
+var root = {
+  val: 9,
+  left: {
+    val: 8,
+    left: 3,
+    right: 1,
+  },
+  right: {
+    val: 6,
+  },
+};
+
+// 删除堆顶元素并保持堆有序
+
+function delete_heap(root) {}
